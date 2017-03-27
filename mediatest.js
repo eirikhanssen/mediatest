4404e20e055e5faa0e82334917129d7b149a316b
Eirik Hanssen <eirikhanssen79@gmail.com>
2017-03-27 15:23:54 +0200

    wip implementing cue sync. halfway broken/working

--- mediatest.js
+++ mediatest.js
@@ -1,3 +1,16 @@
+function time2sec(timestring) {
+				// convert timestring to seconds
+				var timereg = new RegExp(/(\d\d):(\d\d):([0-9.]+)/);
+				if(!timestring.match(timereg)) {
+					return 0;
+				}
+				var sec_in_hours = Number(timestring.replace(timereg, '$1'))*3600;
+				var sec_in_minutes = Number(timestring.replace(timereg, '$2'))*60;
+				var sec_in_seconds = Number(timestring.replace(timereg, '$3'));
+				var sec_total = sec_in_hours + sec_in_minutes + sec_in_seconds;
+				return sec_total;
+			}
+
 function scrollTo(el) {
 	//el.parent().scrollTop(el.offset().top - 30);
 	var scroll_duration = 700;
@@ -36,13 +49,13 @@ var getCurrentSubtitleFromTracks = function (player, kind, srclang, entryIndex) 
 	var tracksObj = player.tracks;
 	for(var i=0; i<tracksObj.length; i++) {
 		var currentTrack = tracksObj[i];
-		if(currentTrack.kind == kind && currentTrack.srclang == srclang) {
+		if(currentTrack.kind == kind && currentTrack.srclang == srclang && player.getCurrentTime() > 0) {
 			//return entryIndex + ' : ' + currentTrack.entries[entryIndex].text;
+            //console.log(entryIndex, srclang, kind, player);
 			return currentTrack.entries[entryIndex].text;
-		}
+		} 
 	}
-	console.log("no entry match: " + kind + ' : ' + srclang + ' : ' + currentIdentifier);
-	return "no entry match: " + kind + ' : ' + srclang + ' : ' + currentIdentifier;
+    return;
 }
 
 var getTranslatedSubtitle = function(player, lang) {
@@ -57,27 +70,44 @@ $( document ).ready(function() {
 		var playerId = $('#audio-' + lang).closest('.mejs__container').attr('id');
 		var player = mejs.players[playerId];
 		var mediaEl = $(this).find('audio')[0];
+        var that = this;
 		//console.log(mediaEl);
+        
+        var updateCueSync = (function() {
+            var updateCuesNo = function() {
+                $(that).find('.cue').each(function() {
+                    //console.log($(this).attr('data-begin'));
+                    //console.log('cues!')
+                    var cue_begin = time2sec($(this).attr('data-begin'));
+                    var cue_end = time2sec($(this).attr('data-end'));
+                    var current_mptime = player.getCurrentTime();
+                    if(cue_begin <= current_mptime && current_mptime <= cue_end) {
+				        if(!$( this ).hasClass("active-cue")) {
+					       $( this ).addClass("active-cue");
+				        } else if ($( this ).hasClass("active-cue")) {
+                            $( this ).removeClass("active-cue");
+                        }
+                    }
+                });
+            }
+            var dummy = function () {
+                return 0;
+            }
+            if(lang=='no') {
+                return updateCuesNo;
+            }
+        })();
+        
+        
 		function updateTextSync(){
 
-			function time2sec(timestring) {
-				// convert timestring to seconds
-				var timereg = new RegExp(/(\d\d):(\d\d):([0-9.]+)/);
-				if(!timestring.match(timereg)) {
-					return 0;
-				}
-				var sec_in_hours = Number(timestring.replace(timereg, '$1'))*3600;
-				var sec_in_minutes = Number(timestring.replace(timereg, '$2'))*60;
-				var sec_in_seconds = Number(timestring.replace(timereg, '$3'));
-				var sec_total = sec_in_hours + sec_in_minutes + sec_in_seconds;
-				return sec_total;
-			}
+			
 			currentPlayerKeys = $('article[data-lang='+lang+ '] .key');
 			otherPlayersKeys = $('article:not([data-lang='+lang+ ']) .key');
 			currentPlayerKeys.each(function(){
-				begin=time2sec($(this).attr('data-begin'));
-				end=time2sec($(this).attr('data-end'));
-				currentTime=player.getCurrentTime();
+				var begin=time2sec($(this).attr('data-begin'));
+				var end=time2sec($(this).attr('data-end'));
+				var currentTime=player.getCurrentTime();
 				if(currentTime < begin || currentTime > end) {
 					if($(this).hasClass('playing')) {
 						$(this).removeClass('playing');
@@ -94,11 +124,12 @@ $( document ).ready(function() {
 					}
 				}
 			});
-
-			var translatedCueText = getTranslatedSubtitle(player, 'no');
+            
+			var translatedCueText = getTranslatedSubtitle(player, 'so');
 			if($('#translation').html() != translatedCueText) {
 				$('#translation').html(translatedCueText);
 			}
+            
 
 		}
 		$(mediaEl).on("playing", function(){
@@ -122,35 +153,11 @@ $( document ).ready(function() {
 		$(mediaEl).on("timeupdate", function(){
 			//console.log("timeupdate of " + lang + ": " + player.getCurrentTime());
 			updateTextSync();
+            updateCueSync();
 			// finn ut hvilken cue som er aktiv
 			// oppdater hvilken cue i andre språk som skal være aktiv/playing
 			// f.eks .sync-no .sync-so .sync-ar .sync-ti
 		});
 	});
 });
-
-
-
-/*
-function audioInit(media_element_id) {
-	var media_el_selector = 
-	var getPlayer = function(media_element_id) {
-		media_element_id = "#" + media_element_id;
-		var playerId = $(media_element_id).closest('.mejs__container').attr('id');	
-		var player = mejs.players[playerId];
-		return player;
-	}
-
-	var getLang = function(media_element_id) {
-		media_element_id = "#" + media_element_id;
-		lang = $(media_element_id).parent().getAttribute()
-	}
-
-	var audioselector = "#" + 
-
-	$('#audio-so audio').on("playing", function(){
-		console.log('playing');
-	});
-
-}*/
 
